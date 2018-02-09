3d30139352c81232ec34f1d72cb3776a50a106e6
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-08 19:52:27 -0500

    Console user's input into search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,10 +2,10 @@
 
 import React, { Component } from 'react';
 
-
+// Capture user's input in input tag (search bar) and console the value
 class SearchBar extends Component {
   render() {
-    return <input />;
+    return <input onChange={event => console.log(event.target.value)} />;
   }
 }
 
