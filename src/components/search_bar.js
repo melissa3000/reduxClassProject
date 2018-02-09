9bbcf7c90d7c8eb096a27ac0f34366feac0c2cab
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-08 20:02:19 -0500

    Add state to class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,19 @@
 
 import React, { Component } from 'react';
 
-// Capture user's input in input tag (search bar) and console the value
+
 class SearchBar extends Component {
+  // Add state to constructor in class component
+  constructor(props) {
+    //call parent method by calling super
+    super(props);
+
+    // Initialize state by creating a new object and assigning it to this.state
+    // include properties you want included on state
+    this.state = { term: ''};
+  }
+
+  // Capture user's input in input tag (search bar) and console the value
   render() {
     return <input onChange={event => console.log(event.target.value)} />;
   }
