f501c49f6e68a839ad6ca73d5342c8284d99cf45
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-09 18:52:20 -0500

    Added YouTube API call

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
+//Top level component is the one that should fetch data
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyC2P9ybRENihTGZcpPqX0gBi7gN5MUwJoY';
 
+// Make YouTube API call for surfboard videos
+YTSearch({ key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
+
 // Create a new component. This component should produce some html.
-// Written in jsx and babel is used to transpile and render the component -
-// it turns the jsx into javascript that can produce html
-
-// Only one component per file
-
-// Take this compenent's generated HTML and put it on the page (in the DOM)
-
-// Passes an instance of App to render
-// container class referenced below is from html doc and is root react element
-
 
 const App = () => {
   return (
