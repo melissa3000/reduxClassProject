c1d7c02802bc6ff365530af81eff3dcd8f4b344d
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-09 18:56:54 -0500

    Refactored functional component into class component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //Top level component is the one that should fetch data
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -8,19 +8,21 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyC2P9ybRENihTGZcpPqX0gBi7gN5MUwJoY';
 
-// Make YouTube API call for surfboard videos
+// Make YouTube API call
 YTSearch({ key: API_KEY, term: 'surfboards'}, function(data) {
   console.log(data);
 });
 
-// Create a new component. This component should produce some html.
-
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+// Refactor from functional component to class component so it can keep track
+// of list of videos by recording them on its state
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 
