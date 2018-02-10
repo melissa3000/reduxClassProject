ff2b4957f8daa9d434dc919ad2ebe170fecda231
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-09 19:14:35 -0500

    Pass video state to video list and capture length of list

--- src/index.js
+++ src/index.js
@@ -8,29 +8,24 @@ import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyC2P9ybRENihTGZcpPqX0gBi7gN5MUwJoY';
 
-// Make YouTube API call
-
-
 
 class App extends Component {
-  // set up constructor for state
   constructor(props){
     super(props);
 
     this.state = { videos: [] };
 
     YTSearch({ key: API_KEY, term: 'kittens'}, (videos) => {
-      // if key and value are the same term, they can be condensed
-      // this.setState({ videos: videos });
       this.setState({ videos });
     });
   }
 
+  // pass data (prop) from App into the video list
   render() {
     return (
       <div>
         <SearchBar />
-        <VideoList />
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
