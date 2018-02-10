09eba378b5db30c5e2db0aeaa984aa7ba993701e
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-09 19:02:19 -0500

    Set up index to auto load videos as part of constructor

--- src/index.js
+++ src/index.js
@@ -9,13 +9,23 @@ import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyC2P9ybRENihTGZcpPqX0gBi7gN5MUwJoY';
 
 // Make YouTube API call
-YTSearch({ key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
 
-// Refactor from functional component to class component so it can keep track
-// of list of videos by recording them on its state
+
+
 class App extends Component {
+  // set up constructor for state
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    // Include search in constructor so that the first search isn't empty and state
+    // will be set right off
+    YTSearch({ key: API_KEY, term: 'kittens'}, (videos) => {
+      this.setState({ videos: videos });
+    });
+  }
+
   render() {
     return (
       <div>
