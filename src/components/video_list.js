e30f3c4041ec27a26903f1b489adb41dca1ccc78
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-10 18:53:11 -0500

    Display list of videos from search results on DOM

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+  // For each element of videos a function will get called with a single video and
+  // return a video list item and pass it the video as a property named video
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video={video} />
+  });
+
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 };
