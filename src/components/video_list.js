1101d66bf3d611cf837350b8132bd1864d92d949
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-10 18:58:11 -0500

    Add keys to each video list item

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -5,7 +5,7 @@ const VideoList = (props) => {
   // For each element of videos a function will get called with a single video and
   // return a video list item and pass it the video as a property named video
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem video={video} />
+    return <VideoListItem key={video.etag} video={video} />
   });
 
   return (
