0b4b263b051dbc0e7346f4d0b3d6a12f96ce8ac2
Melissa Boyer <melissaboyer3000@gmail.com>
2018-02-10 19:10:00 -0500

    Display video thumbnail and title in search results

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,6 +4,8 @@ import VideoListItem from './video_list_item';
 const VideoList = (props) => {
   // For each element of videos a function will get called with a single video and
   // return a video list item and pass it the video as a property named video
+
+  // use etag as a key becuase it is unique
   const videoItems = props.videos.map((video) => {
     return <VideoListItem key={video.etag} video={video} />
   });
